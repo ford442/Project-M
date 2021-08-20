6b25fdcd45b6b7e92089a4b552717bdcbba7e6e9
Noah Cohn <noahc42@gmail.com>
2021-08-20 05:59:24 -0700

    Update filesys.js

--- src/projectM-emscripten/filesys.js
+++ src/projectM-emscripten/filesys.js
@@ -6,14 +6,11 @@ document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
 document.getElementById("dis").click();
-
 });
-
 document.getElementById('btn2').addEventListener('click',function(){
-
-FS.mkdir('/presets');
+let pth=document.getElementById('path').innerHTML;
 let ff=new XMLHttpRequest();
-ff.open("GET","./presets/rose.milk",true);
+ff.open("GET",pth,true);
 ff.responseType="arraybuffer";
 ff.onload=function(oEvent){
 var arrayBuffer=ff.response;
@@ -24,8 +21,6 @@ console.log('File written to /presets/tst.milk.');
 }}
 ff.send(null);
 });
-
-
 document.getElementById('btn').addEventListener('click',function(){
 Module.ccall('chng');
 });

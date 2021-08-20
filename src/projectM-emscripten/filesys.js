5cf9ce464c335dbb84afb30b05967c8ce992a41f
Noah Cohn <noahc42@gmail.com>
2021-08-20 01:43:10 -0700

    Create filesys.js

---
+++ src/projectM-emscripten/filesys.js
@@ -0,0 +1,9 @@
+let fll=new BroadcastChannel('file');
+fll.addEventListener('message',ea=> {
+let fill=new Uint8Array(ea.data.data);
+FS.writeFile('/sample.wav',fill);
+document.getElementById("ihig").innerHTML=window.innerHeight;
+document.getElementById("circle").height=window.innerHeight;
+document.getElementById("circle").width=window.innerWidth;
+document.getElementById("di").click();
+});

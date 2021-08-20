40b80e81e8e5e8dbc3d4c246e9cfc49c87cca55e
Noah Cohn <noahc42@gmail.com>
2021-08-20 01:24:28 -0700

    Update wav.cpp

--- src/projectM-emscripten/wav.cpp
+++ src/projectM-emscripten/wav.cpp
@@ -59,7 +59,7 @@ case SDL_QUIT: app.done = true;
 break;
 }
 
-app.pm->pcm()->addPCMfloat_2ch(stm,4096);
+app.pm->pcm()->addPCM8_512(stm);
   
 glClearColor(0.0, 0.0, 0.0, 0.0);
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);

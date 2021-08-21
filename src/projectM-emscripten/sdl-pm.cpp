5beae4fcdd1e1899c720c1f564d7e28bd30d60de
Noah Cohn <noahc42@gmail.com>
2021-08-21 01:27:59 -0700

    Update sdl-pm.cpp

--- src/projectM-emscripten/sdl-pm.cpp
+++ src/projectM-emscripten/sdl-pm.cpp
@@ -18,9 +18,9 @@ SDL_GLContext *glCtx;
 bool done;
 projectM::Settings settings;
 SDL_AudioDeviceID audioInputDevice;
-} projectMApp;
+}
+projectMApp;
 projectMApp app;
-
 void renderFrame()
 {
 app.pm->pcm()->addPCM16Data(reinterpret_cast<short*>(stm),len/sizeof(short)/2);
@@ -31,12 +31,12 @@ glFlush();
 SDL_GL_SwapWindow(app.win);
 }
 extern "C" {
-static struct{SDL_AudioSpec spec;uint8_t *snd;slen;int pos;}wave;
+static struct{SDL_AudioSpec spec;uint8_t *snd;uint8_t slen;int pos;}wave;
 static SDL_AudioDeviceID dev;
 static void cls_aud(){if(dev!=0){SDL_PauseAudioDevice(dev,SDL_TRUE);SDL_CloseAudioDevice(dev);dev=0;}}
 static void qu(int rc){SDL_Quit();exit(rc);}
 static void opn_aud(){dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);if(!dev){SDL_FreeWAV(wave.snd);qu(2);}SDL_PauseAudioDevice(dev,SDL_FALSE);}
-void SDLCALL bfr(void *unused,stm,len){uint8_t *wptr;int lft;wptr=wave.snd+wave.pos;lft=wave.slen-wave.pos;
+void SDLCALL bfr(void *unused,uint8_t *stm,int len){uint8_t *wptr;int lft;wptr=wave.snd+wave.pos;lft=wave.slen-wave.pos;
 while (lft<=len){SDL_memcpy(stm,wptr,lft);stm+=lft;len-=lft;wptr=wave.snd;lft=wave.slen;wave.pos=0;}
 SDL_memcpy(stm,wptr,len);wave.pos+=len;}
 void pl(){cls_aud();char flnm[4096];

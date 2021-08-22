4c59e443e045a9324aff48452e9a67f06cc961ea
Noah Cohn <noahc42@gmail.com>
2021-08-21 22:09:43 -0700

    Update sdl-pm.cpp

--- src/projectM-emscripten/sdl-pm.cpp
+++ src/projectM-emscripten/sdl-pm.cpp
@@ -29,7 +29,7 @@ for(int i=0;i<512;i++){
 for(int j=0;j<2;j++){
 pcm16[j][i]=stm[i+j];
 }}
-app->pcm()->addPCM16(pcm16);
+app.pm->pcm()->addPCM16(pcm16);
 }glClearColor(0.0, 0.5, 0.0, 0.0);
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 app.pm->renderFrame();

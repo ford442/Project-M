26fc7409f57cd54a04ae5ba5c72b38c3484974ba
Noah Cohn <noahc42@gmail.com>
2021-08-25 00:36:16 -0700

    Update audio.cpp

--- src/projectM-emscripten/audio.cpp
+++ src/projectM-emscripten/audio.cpp
@@ -23,7 +23,7 @@ projectMApp;
 projectMApp app;
 void renderFrame()
 {
-auto sndat=reinterpret_cast<float*>(wave.snd);
+auto sndat=reinterpret_cast<short*>(wave.snd);
 app.pm->pcm()->addPCM16Data(sndat,wave.slen);      
 glClearColor(0.0, 0.5, 0.0, 0.0);
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);

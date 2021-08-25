df0ab4db575b2371a52ecd8f5c866ada9e23f301
Noah Cohn <noahc42@gmail.com>
2021-08-25 03:12:01 -0700

    Update filesys.js

--- src/projectM-emscripten/filesys.js
+++ src/projectM-emscripten/filesys.js
@@ -9,6 +9,8 @@ document.getElementById("dis").click();
 });
 document.getElementById('btn2').addEventListener('click',function(){
 let pth=document.getElementById('path').innerHTML;
+let pth2=document.getElementById('path2').innerHTML;
+let pth3=document.getElementById('path3').innerHTML;
 let ff=new XMLHttpRequest();
 ff.open("GET",pth,true);
 ff.responseType="arraybuffer";
@@ -16,32 +18,30 @@ ff.onload=function(oEvent){
 var arrayBuffer=ff.response;
 if(arrayBuffer){
 var fil=new Uint8ClampedArray(arrayBuffer);
-FS.writeFile('/presets/tst.milk',fil);
-console.log('File written to /presets/set1.milk.');
+FS.writeFile('/presets/set1.milk',fil);
+console.log('File: set1.milk.');
 }}
 ff.send(null);
-  let pth=document.getElementById('path2').innerHTML;
 let ff=new XMLHttpRequest();
-ff.open("GET",pth,true);
+ff.open("GET",pth2,true);
 ff.responseType="arraybuffer";
 ff.onload=function(oEvent){
 var arrayBuffer=ff.response;
 if(arrayBuffer){
 var fil=new Uint8ClampedArray(arrayBuffer);
 FS.writeFile('/presets/set2.milk',fil);
-console.log('File written to /presets/tst.milk.');
+console.log('File: /set2.milk.');
 }}
 ff.send(null);
-  let pth=document.getElementById('path3').innerHTML;
 let ff=new XMLHttpRequest();
-ff.open("GET",pth,true);
+ff.open("GET",pth3,true);
 ff.responseType="arraybuffer";
 ff.onload=function(oEvent){
 var arrayBuffer=ff.response;
 if(arrayBuffer){
 var fil=new Uint8ClampedArray(arrayBuffer);
 FS.writeFile('/presets/set3.milk',fil);
-console.log('File written to /presets/tst.milk.');
+console.log('File: set3.milk.');
 }}
 ff.send(null);
 });

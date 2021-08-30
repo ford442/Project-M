1d0525671c46d70c15ecce957621ff6a72b2b38d
Noah Cohn <noahc42@gmail.com>
2021-08-30 06:26:10 -0700

    Update ccall.js

--- src/projectM-emscripten/ccall.js
+++ src/projectM-emscripten/ccall.js
@@ -15,7 +15,7 @@ e.preventDefault();
 pos3=e.clientX;
 pos4=e.clientY;
 var cords=[pos3,pos4];
-Module.ccall("tchd",null,[number,number],cords);
+Module.ccall("tchd",null,['number','number'],cords);
 document.onmouseup=closeDragElement();
 };
 document.getElementById('canvas').onmousedown=dragMouseDown();
@@ -24,5 +24,5 @@ function showCoords(event){
 var x=event.clientX;
 var y=event.clientY;
 var coords=[x,y];
-Module.ccall("tch",null,[number,number],coords);
+Module.ccall("tch",null,['number','number'],coords);
 }});

457f40a2917755d41016f16864962b6a00875032
Noah Cohn <noahc42@gmail.com>
2021-09-02 23:05:55 -0700

    Update filesys.js

--- src/projectM-emscripten/filesys.js
+++ src/projectM-emscripten/filesys.js
@@ -1,11 +1,11 @@
 let fll=new BroadcastChannel('file');
 fll.addEventListener('message',ea=> {
-let fill=new Uint8Array(ea.data.data);
-FS.writeFile('/sample.wav',fill);
 document.getElementById("ihig").innerHTML=window.innerHeight;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("circle").width=window.innerWidth;
 document.getElementById("dis").click();
+let fill=new Uint8Array(ea.data.data);
+FS.writeFile('/sample.wav',fill);
 });
 document.getElementById('btn2').addEventListener('click',function(){
 let pth=document.getElementById('path').innerHTML;

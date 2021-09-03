a851db24bca01d3cf77ffb0209d448a403e80c82
Noah Cohn <noahc42@gmail.com>
2021-09-03 04:22:58 -0700

    Update audio.cpp

--- src/projectM-emscripten/audio.cpp
+++ src/projectM-emscripten/audio.cpp
@@ -6,11 +6,11 @@
 #include <GLES3/gl3.h>
 #include "SDL2/SDL_config.h"
 #include <SDL2/SDL.h>
-
 const float FPS = 60;
 static SDL_AudioDeviceID dev;
 static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
-typedef struct{
+typedef struct
+{
 projectM *pm;
 SDL_Window *win;
 SDL_GLContext *glCtx;
@@ -20,11 +20,12 @@ SDL_AudioDeviceID dev;
 }
 projectMApp;
 projectMApp app;
-void renderFrame(){
+void renderFrame()
+{
 auto sndat=reinterpret_cast<short*>(&wave.snd);
 unsigned int ll=sizeof(&wave.snd);
 app.pm->pcm()->addPCM16Data(sndat,ll);
-glClearColor(1.0, 1.0, 1.0, 0.5);
+glClearColor(0.0, 0.0, 0.0, 0.0);
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
@@ -52,9 +53,7 @@ void tchdsta(int x, int y){
 app.pm->touchDestroyAll();
 }  
 void chng(){
-int width = EM_ASM_INT({
-return document.getElementById('ihig').innerHTML;
-});
+int width = EM_ASM_INT({return document.getElementById('ihig').innerHTML;});
 int height = width;
 app.win = SDL_CreateWindow("Bat files", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,width, height, SDL_WINDOW_OPENGL);
 SDL_GLContext glCtx = SDL_GL_CreateContext(app.win);
@@ -68,10 +67,8 @@ app.settings.fps = FPS;
 app.settings.textureSize = 2048;
 app.settings.windowWidth = width;
 app.settings.windowHeight = width;
-app.settings.smoothPresetDuration = 1;
-app.settings.presetDuration = EM_ASM_INT({
-return document.getElementById('dura').innerHTML;
-});
+app.settings.smoothPresetDuration = 3;
+app.settings.presetDuration = EM_ASM_INT({return document.getElementById('dura').innerHTML;});
 app.settings.beatSensitivity = 1;
 app.settings.aspectCorrection = 0;
 app.settings.easterEgg = 0;
@@ -81,39 +78,39 @@ app.settings.presetURL = "/presets";
 app.pm = new projectM(app.settings);
 printf("Init ProjectM\n");
 app.pm->selectRandom(true);
+printf("Select random preset.\n");
 app.pm->projectM_resetGL(width, height);
+printf("Reseting GL.\n");
 DIR *m_dir;
-if ((m_dir = opendir("/")) == NULL){
+if ((m_dir = opendir("/")) == NULL)
+{
 printf("error opening /\n");
-}else{
+}
+else
+{
 struct dirent *dir_entry;
-while ((dir_entry = readdir(m_dir)) != NULL){
+while ((dir_entry = readdir(m_dir)) != NULL)
+{
 printf("%s\n", dir_entry->d_name);
 }}
-for (uint i = 0; i < app.pm->getPlaylistSize(); i++){
+for (uint i = 0; i < app.pm->getPlaylistSize(); i++)
+{
 printf("%d\t%s\n", i, app.pm->getPresetName(i).c_str());
 }
 emscripten_set_main_loop((void (*)())renderFrame, 0, 0);
 }
 static void cls_aud(){if(dev!=0){
-SDL_PauseAudioDevice(dev,SDL_TRUE);
-SDL_CloseAudioDevice(dev);dev=0;
+SDL_PauseAudioDevice(dev,SDL_TRUE);SDL_CloseAudioDevice(dev);dev=0;
 }}
-static void qu(int rc){
-SDL_Quit();exit(rc);
+static void qu(int rc){SDL_Quit();exit(rc);
 }
-static void opn_aud(){
-dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
-if(!dev){SDL_FreeWAV(wave.snd);
-qu(2);
-}
-SDL_PauseAudioDevice(dev,SDL_FALSE);
+static void opn_aud(){dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){SDL_FreeWAV(wave.snd);qu(2);}SDL_PauseAudioDevice(dev,SDL_FALSE);
 }
 void SDLCALL bfr(void *unused,Uint8 * stm,int len){
 Uint8 *wptr;
 int lft;
-wptr=wave.snd+wave.pos;
-lft=wave.slen-wave.pos;
+wptr=wave.snd+wave.pos;lft=wave.slen-wave.pos;
 while (lft<=len){
 SDL_memcpy(stm,wptr,lft);
 stm+=lft;
@@ -125,25 +122,23 @@ wave.pos=0;
 SDL_memcpy(stm,wptr,len);
 wave.pos+=len;
 }
-void pl(){
-cls_aud();
-char flnm[256];
+void pl(){cls_aud();
+char flnm[1024];
 SDL_FreeWAV(wave.snd);
 SDL_Quit();
 SDL_SetMainReady();
-if (SDL_Init(SDL_INIT_AUDIO)<0){
-qu(1);
-}
+if (SDL_Init(SDL_INIT_AUDIO)<0){qu(1);}
 SDL_strlcpy(flnm,"/sample.wav",sizeof(flnm));
-if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
-qu(1);
-}
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){qu(1);}
 wave.pos=0;
 wave.spec.callback=bfr;
 opn_aud();
 }}
-int main(){
-EM_ASM(FS.mkdir('/presets'););
+int main()
+{
+EM_ASM(
+FS.mkdir('/presets');
+);
 app.done = 0;
 SDL_Init(SDL_INIT_VIDEO);
 return PROJECTM_SUCCESS;

c21034df07b5577858f0a11c96b113db271cbb13
Noah Cohn <noahc42@gmail.com>
2021-09-07 12:27:19 -0700

    Update thread.cpp

--- src/projectM-emscripten/thread.cpp
+++ src/projectM-emscripten/thread.cpp
@@ -153,10 +153,7 @@ FS.mkdir('/presets');let fll=new BroadcastChannel('file');
 fll.addEventListener('message',ea=> {
 let fill=new Uint8Array(ea.data.data);
 FS.writeFile('/sample.wav',fill);
-document.getElementById("ihig").innerHTML=window.innerHeight;
-document.getElementById("circle").height=window.innerHeight;
-document.getElementById("circle").width=window.innerWidth;
-document.getElementById("dis").click();
+Module.ccall("pl");
 });
 document.getElementById('btn2').addEventListener('click',function(){
 let pth=document.getElementById('path').innerHTML;

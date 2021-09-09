3c2c11f5f12b502aa0ff8b0800ebf31248622571
Noah Cohn <noahc42@gmail.com>
2021-09-09 06:40:05 -0700

    Update PerPixelMesh.hpp

--- src/libprojectM/Renderer/PerPixelMesh.hpp
+++ src/libprojectM/Renderer/PerPixelMesh.hpp
@@ -2,44 +2,29 @@
 #define PerPixelMesh_HPP
 
 #include <vector>
-
-struct PixelPoint
-{
-	float x;
-	float y;
-
-    PixelPoint(float _x, float _y);
+struct PixelPoint{
+float x;
+float y;
+PixelPoint(float _x,float _y);
 };
-
-struct PerPixelContext
-{
-	float x;
-	float y;
-	float rad;
-	float theta;
-
-	int i;
-	int j;
-
-    PerPixelContext(float x, float _y, float _rad, float _theta, int _i, int _j);
+struct PerPixelContext{
+float x;
+float y;
+float rad;
+float theta;
+int i;
+int j;
+PerPixelContext(float x,float _y,float _rad,float _theta,int _i,int _j);
 };
-
-class PerPixelMesh
-{
+class PerPixelMesh{
 public:
-	int width;
-	int height;
-	int size;
-
-	std::vector<PixelPoint> p;
-	std::vector<PixelPoint> p_original;
-	std::vector<PerPixelContext> identity;
-
-    PerPixelMesh(int _width, int _height);
-
-	void Reset();
+int width;
+int height;
+int size;
+std::vector<PixelPoint> p;
+std::vector<PixelPoint> p_original;
+std::vector<PerPixelContext> identity;
+PerPixelMesh(int _width,int _height);
+void Reset();
 };
-
-
-
 #endif

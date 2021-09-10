0a5ffa0fe837cafc80b71d8853d28e348535a35c
Noah Cohn <noahc42@gmail.com>
2021-09-09 19:41:15 -0700

    Update Renderer.cpp

--- src/libprojectM/Renderer/Renderer.cpp
+++ src/libprojectM/Renderer/Renderer.cpp
@@ -401,7 +401,7 @@ glCullFace(GL_BACK);
 #ifndef GL_TRANSITION
 glEnable(GL_LINE_SMOOTH);
 #endif
-glClearColor(0,0,0,1);
+glClearColor(0,0,0,0);
 glViewport(vstartx,vstarty,w,h);
 glEnable(GL_BLEND);
 texsizeX=w;

25e97d51924f388b8944143d228fdb5d3424607a
Noah Cohn <noahc42@gmail.com>
2021-09-16 23:12:33 -0700

    Update noclr.cpp

--- src/projectM-emscripten/noclr.cpp
+++ src/projectM-emscripten/noclr.cpp
@@ -15,7 +15,7 @@ auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 unsigned int ll=sizeof(sndBuf);
 app.pm->pcm()->addPCM16Data(sndat,ll);
-glClear(GL_COLOR_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
 SDL_GL_SwapWindow(app.win);

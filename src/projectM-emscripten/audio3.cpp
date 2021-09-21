be79aabbe58730fd56a868d874344a8a35fad494
Noah Cohn <noahc42@gmail.com>
2021-09-21 16:29:09 -0700

    Update audio3.cpp

--- src/projectM-emscripten/audio3.cpp
+++ src/projectM-emscripten/audio3.cpp
@@ -32,7 +32,7 @@ static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 app.pm->pcm()->addPCM16Data(sndat,1024);
-glClearColor(1.0,1.0,1.0,0.5);
+// glClearColor(1.0,1.0,1.0,0.5);
 glClear(GL_COLOR_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
@@ -113,6 +113,7 @@ printf("%s\n",dir_entry->d_name);
 for(uint i=0;i<app.pm->getPlaylistSize();i++){
 printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
+glClearColor(0.0,0.0,0.0,0.0)
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
 static void cls_aud(){

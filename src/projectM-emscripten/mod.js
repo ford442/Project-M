d62c9a410f5cd1b8dfc1e4a50ed9c67cb67c0879
Noah Cohn <noahc42@gmail.com>
2021-09-20 19:53:45 -0700

    Create mod.js

---
+++ src/projectM-emscripten/mod.js
@@ -0,0 +1,10 @@
+var Module =
+{
+    canvas: (function() {
+    var aCanvas = document.getElementById('canvas');
+    return aCanvas;
+  })(),
+
+};
+
+const OccViewerModuleInitialized = createModule(Module);

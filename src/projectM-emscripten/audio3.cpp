fae567584721d0fec7fea29de49e78607f54fbec
Noah Cohn <noahc42@gmail.com>
2021-09-21 14:24:46 -0700

    Update audio3.cpp

--- src/projectM-emscripten/audio3.cpp
+++ src/projectM-emscripten/audio3.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +7,22 @@
 #include <GLES3/gl3.h>
 #include "SDL2/SDL_config.h"
 #include <SDL2/SDL.h>
+#include <EGL/egl.h>
+
+static EGLint attribute_list[]={
+EGL_RED_SIZE,8,
+EGL_GREEN_SIZE,8,
+EGL_BLUE_SIZE,8,
+EGL_ALPHA_SIZE,8,
+EGL_DEPTH_SIZE,0,
+EGL_STENCIL_SIZE,0,
+EGL_BUFFER_SIZE,32,
+EGL_CONFORMANT,EGL_OPENGL_ES2_BIT,
+EGL_BIND_TO_TEXTURE_RGBA,EGL_TRUE,
+EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
+EGL_NONE
+};
+
 const float FPS=60.0;
 static SDL_AudioDeviceID dev;
 static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
@@ -29,14 +46,41 @@ app.pm->setPresetLock(true);
 printf("Preset locked.\n");
 }
 static void chngt(){
+  
+  
+EmscriptenWebGLContextAttributes attr;
+attr.alpha = 1;
+attr.depth = 0;
+attr.antialias = 0;
+attr.premultipliedAlpha = 0;
+attr.preserveDrawingBuffer = 0;
+emscripten_webgl_init_context_attributes(&attr);
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#canvas",&attr);
+emscripten_webgl_make_context_current(ctx);
+EGLConfig eglconfig=NULL;
+EGLint config_size,major,minor;
+EGLContext contextegl;
+EGLDisplay display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
+eglInitialize(display,&major,&minor);
+if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
+if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
+}
+EGLint anEglCtxAttribs2[]={EGL_CONTEXT_CLIENT_VERSION,3,EGL_NONE,EGL_NONE};
+contextegl=eglCreateContext (display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
+if(contextegl==EGL_NO_CONTEXT){
+}else{
+EGLSurface surface=eglCreateWindowSurface(display,eglconfig,NULL,NULL);
+eglMakeCurrent(display,surface,surface,contextegl);
+}
+  
 emscripten_cancel_main_loop();
-SDL_SetMainReady();
-SDL_Init(SDL_INIT_VIDEO);
+// SDL_SetMainReady();
+// SDL_Init(SDL_INIT_VIDEO);
 int width=EM_ASM_INT({return document.getElementById('ihig').innerHTML;});
 int height=width;
 app.win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
-SDL_GLContext glCtx=SDL_GL_CreateContext(app.win);
-app.glCtx=&glCtx;
+// SDL_GLContext glCtx=SDL_GL_CreateContext(app.win);
+app.glCtx=&contextegl;
 SDL_SetWindowTitle(app.win,"1inkDrop - [from 1ink.us]");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));

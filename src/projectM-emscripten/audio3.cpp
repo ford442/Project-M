900880bb3e68c6159bc7d96d24cd73841443e38d
Noah Cohn <noahc42@gmail.com>
2021-09-21 17:52:10 -0700

    Update audio3.cpp

--- src/projectM-emscripten/audio3.cpp
+++ src/projectM-emscripten/audio3.cpp
@@ -113,7 +113,7 @@ printf("%s\n",dir_entry->d_name);
 for(uint i=0;i<app.pm->getPlaylistSize();i++){
 printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
-glClearColor(0.0,0.0,0.0,0.0);
+glClearColor(1.0,1.0,0.99,0.0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
 static void cls_aud(){

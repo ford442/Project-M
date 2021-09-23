bf3035938a844770fdfe337a8af011d41da78a18
Noah Cohn <noahc42@gmail.com>
2021-09-23 01:50:14 -0700

    Update egl4.cpp

--- src/projectM-emscripten/egl4.cpp
+++ src/projectM-emscripten/egl4.cpp
@@ -33,7 +33,7 @@ auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 app.pm->pcm()->addPCM16Data(sndat,1024);
 app.pm->renderFrame();
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 glFlush();
 SDL_GL_SwapWindow(app.win);
 }

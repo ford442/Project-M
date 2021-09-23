f38971554044a6967c924f44a0d7f9b9dbfb82d2
Noah Cohn <noahc42@gmail.com>
2021-09-23 01:48:21 -0700

    Update egl4.cpp

--- src/projectM-emscripten/egl4.cpp
+++ src/projectM-emscripten/egl4.cpp
@@ -15,7 +15,7 @@ EGL_RED_SIZE,8,
 EGL_GREEN_SIZE,8,
 EGL_BLUE_SIZE,8,
 EGL_ALPHA_SIZE,8,
-EGL_DEPTH_SIZE,32,
+EGL_DEPTH_SIZE,0,
 EGL_STENCIL_SIZE,8,
 EGL_BUFFER_SIZE,32,
 EGL_CONFORMANT,EGL_OPENGL_ES2_BIT,
@@ -32,8 +32,8 @@ static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 app.pm->pcm()->addPCM16Data(sndat,1024);
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 app.pm->renderFrame();
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 glFlush();
 SDL_GL_SwapWindow(app.win);
 }
@@ -48,7 +48,7 @@ printf("Preset locked.\n");
 static void chngt(){
 EmscriptenWebGLContextAttributes attr;
 attr.alpha = 1;
-attr.depth = 1;
+attr.depth = 0;
 attr.stencil = 1;
 attr.antialias = 0;
 attr.premultipliedAlpha = 0;
@@ -95,7 +95,7 @@ app.settings.beatSensitivity=1;
 app.settings.aspectCorrection=0;
 app.settings.easterEgg=0;
 app.settings.shuffleEnabled=0;
-app.settings.softCutRatingsEnabled=0;
+app.settings.softCutRatingsEnabled=1;
 app.settings.presetURL="/presets";
 app.pm=new projectM(app.settings);
 printf("Init ProjectM\n");
@@ -115,9 +115,9 @@ printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
 glClearColor(0,0,0,0);
 glStencilMask(1);
-glDepthMask(1);
+// glDepthMask(1);
 glClearStencil(0);
-glClearDepthf(0);
+// glClearDepthf(0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
 static void cls_aud(){
@@ -185,7 +185,9 @@ void swtch(){
 swtcht();
 }}
 int main(){
-// EM_ASM({FS.mkdir('/presets');});
+EM_ASM({
+FS.mkdir('/presets');
+});
 app.done=0;
 return 1;
 }

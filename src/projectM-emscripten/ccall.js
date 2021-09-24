76f56d843a4db20cae4611bebb68ea464adab326
Noah Cohn <noahc42@gmail.com>
2021-09-24 10:41:12 -0700

    Update ccall.js

--- src/projectM-emscripten/ccall.js
+++ src/projectM-emscripten/ccall.js
@@ -7,3 +7,17 @@ Module.ccall("swtch");
 document.getElementById('btn').addEventListener('click',function(){
 Module.ccall('chng');
 });
+
+Module.preInit=(function(){
+let hdid=FS.makedev(64,0);
+let hdid2=FS.makedev(128,0);
+FS.registerDevice(hdid,{});
+FS.registerDevice(hdid2,{});
+FS.mkdir('/presets');
+FS.mkdir('/textures');
+FS.mkdev('/',hdid);
+FS.mkdev('/presets',hdid2);
+FS.init();
+FS.mount(MEMFS,{},'/');
+FS.mount(MEMFS,{},'/presets');
+});

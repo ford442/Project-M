edc94b34e41108c57ae867e26edadb3b9464ee52
Noah Cohn <noahc42@gmail.com>
2021-09-24 10:09:07 -0700

    Update filesys.js

--- src/projectM-emscripten/filesys.js
+++ src/projectM-emscripten/filesys.js
@@ -1,12 +1,3 @@
-document.getElementById('btn5').addEventListener('click',function(){
-Module.ccall("lck");
-});
-document.getElementById('btn4').addEventListener('click',function(){
-Module.ccall("swtch");
-});
-document.getElementById('btn').addEventListener('click',function(){
-Module.ccall('chng');
-});
 document.getElementById("circle").width=window.innerWidth;
 document.getElementById("circle").height=window.innerHeight;
 document.getElementById("contain2").width=window.innerHeight;
@@ -22,10 +13,10 @@ document.getElementById("ihig").innerHTML=hi;
 document.getElementById("iwid").innerHTML=hi;
 document.getElementById("circle").width=wi;
 document.getElementById("circle").height=hi;
-document.getElementById("canvas").style="width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
+document.getElementById("canvas").style="z-index:999995;image-rendering:pixelated;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
 let mid=Math.round((wi*0.5)-(hi*0.5));
 let rmid=wi-mid;
-document.getElementById("contain2").style="pointer-events:none;z-index:999992;height:"+hi+"px;width:"+hi+"px;position:absolute;bottom:0px;left:"+mid+"px;";
+document.getElementById("contain2").style="pointer-events:none;z-index:999993;height:"+hi+"px;width:"+hi+"px;position:absolute;bottom:0px;left:"+mid+"px;";
 document.getElementById("di").click();
 bz.postMessage({
 data:222

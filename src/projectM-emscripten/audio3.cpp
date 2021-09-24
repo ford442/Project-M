c4586cb4389a478a0c5abff42eb0bcd96c70d881
Noah Cohn <noahc42@gmail.com>
2021-09-24 12:24:55 -0700

    Update audio3.cpp

--- src/projectM-emscripten/audio3.cpp
+++ src/projectM-emscripten/audio3.cpp
@@ -28,9 +28,8 @@ projectMApp;projectMApp app;
 static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
-glClear(GL_STENCIL_BUFFER_BIT);
-glClear(GL_COLOR_BUFFER_BIT);
 app.pm->pcm()->addPCM16Data(sndat,1024);
+glClear(GL_COLOR_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
 SDL_GL_SwapWindow(app.win);

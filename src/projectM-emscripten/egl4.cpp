e6c7a6a7e82695f877ff956ebf937f7ff9ee6905
Noah Cohn <noahc42@gmail.com>
2021-09-24 13:45:02 -0700

    Update egl4.cpp

--- src/projectM-emscripten/egl4.cpp
+++ src/projectM-emscripten/egl4.cpp
@@ -15,9 +15,8 @@ EGL_RED_SIZE,8,
 EGL_GREEN_SIZE,8,
 EGL_BLUE_SIZE,8,
 EGL_ALPHA_SIZE,8,
-EGL_DEPTH_SIZE,0,
-EGL_STENCIL_SIZE,8,
-EGL_BUFFER_SIZE,32,
+EGL_CONFORMANT,EGL_OPENGL_ES2_BIT,
+EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
 EGL_NONE
 };
 const float FPS=60;
@@ -28,8 +27,8 @@ projectMApp;projectMApp app;
 static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
-glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 app.pm->pcm()->addPCM16Data(sndat,1024);
+glClear(GL_COLOR_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
 SDL_GL_SwapWindow(app.win);
@@ -45,8 +44,6 @@ printf("Preset locked.\n");
 static void chngt(){
 EmscriptenWebGLContextAttributes attr;
 attr.alpha = 1;
-attr.depth = 0;
-attr.stencil = 1;
 attr.antialias = 0;
 attr.premultipliedAlpha = 0;
 attr.preserveDrawingBuffer = 0;
@@ -68,25 +65,21 @@ if(contextegl==EGL_NO_CONTEXT){
 EGLSurface surface=eglCreateWindowSurface(display,eglconfig,NULL,NULL);
 eglMakeCurrent(display,surface,surface,contextegl);
 }}
-// emscripten_cancel_main_loop();
-// SDL_SetMainReady();
-// SDL_Init(SDL_INIT_VIDEO);
 int width=EM_ASM_INT({return document.getElementById('ihig').innerHTML;});
 int height=width;
 app.win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
-// SDL_GLContext glCtx=SDL_GL_CreateContext(app.win);
 app.glCtx=&contextegl;
 SDL_SetWindowTitle(app.win,"1inkDrop - [from 1ink.us]");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GL_SHADING_LANGUAGE_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-app.settings.meshX=60;
-app.settings.meshY=60;
+app.settings.meshX=64;
+app.settings.meshY=64;
 app.settings.textureSize=1024;
 app.settings.fps=FPS;
 app.settings.textureSize=EM_ASM_INT({return Math.pow(2,Math.floor(Math.log(window.innerHeight)/Math.log(2)));});
 app.settings.windowWidth=width;
 app.settings.windowHeight=width;
-app.settings.smoothPresetDuration=4;
+app.settings.smoothPresetDuration=22;
 app.settings.presetDuration=88;
 app.settings.beatSensitivity=1;
 app.settings.aspectCorrection=0;
@@ -110,9 +103,7 @@ printf("%s\n",dir_entry->d_name);
 for(uint i=0;i<app.pm->getPlaylistSize();i++){
 printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
-glClearColor(0,0,0,0);
-glStencilMask(1);
-glClearStencil(0);
+glClearColor(1,1,1,0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
 static void cls_aud(){
@@ -182,7 +173,13 @@ swtcht();
 int main(){
 EM_ASM({
 FS.mkdir('/presets');
-});
+document.getElementById("circle").width=window.innerWidth;
+document.getElementById("circle").height=window.innerHeight;
+document.getElementById("contain2").width=window.innerHeight;
+document.getElementById("contain2").height=window.innerHeight;
+document.getElementById('btn3').addEventListener('click',function(){
+window.open('https://test.1ink.us/libflac.js/');
+});});
 app.done=0;
 return 1;
 }

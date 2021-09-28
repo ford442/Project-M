0007587c7aeb2498cae28d9138f4395ad27f21e7
Noah Cohn <noahc42@gmail.com>
2021-09-28 00:24:18 -0700

    Update test.cpp

--- src/projectM-emscripten/test.cpp
+++ src/projectM-emscripten/test.cpp
@@ -16,6 +16,10 @@ EGL_GREEN_SIZE,32,
 EGL_BLUE_SIZE,32,
 EGL_ALPHA_SIZE,32,
 EGL_BUFFER_SIZE,128,
+EGL_DEPTH_SIZE,32,
+EGL_STENCIL_SIZE,32,
+EGL_CONFORMANT,EGL_OPENGL_ES3_BIT,
+EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
 EGL_NONE
 };
 
@@ -27,11 +31,9 @@ projectMApp;projectMApp app;
 static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
+app.pm->pcm()->addPCM16Data(sndat,1024);
 glClearColor(0.0,0.0,0.0,0.0);
 glClear(GL_COLOR_BUFFER_BIT);
-
-app.pm->pcm()->addPCM16Data(sndat,1024);
-
 app.pm->renderFrame();
 glFlush();
 SDL_GL_SwapWindow(app.win);
@@ -47,8 +49,8 @@ printf("Preset locked.\n");
 static void chngt(){
 EmscriptenWebGLContextAttributes attr;
 attr.alpha = 1;
-attr.stencil = 0;
-attr.depth = 0;
+attr.stencil = 1;
+attr.depth = 1;
 attr.antialias = 0;
 attr.premultipliedAlpha = 0;
 attr.preserveDrawingBuffer = 0;

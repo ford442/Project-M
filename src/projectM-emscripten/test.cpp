74405cca169a5efa8892fdf29ead78ac66d18ded
Noah Cohn <noahc42@gmail.com>
2021-09-28 00:37:03 -0700

    Update test.cpp

--- src/projectM-emscripten/test.cpp
+++ src/projectM-emscripten/test.cpp
@@ -33,7 +33,7 @@ auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 glColorMask(0,0,0,1);
 glClearColor(0.0,0.0,0.0,0.0);
-glClear(GL_COLOR_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 glColorMask(1,1,1,1);
 app.pm->pcm()->addPCM16Data(sndat,1024);
 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);

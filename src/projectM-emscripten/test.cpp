af970b01a9ca420487c00b0111643c30cc7c08b5
Noah Cohn <noahc42@gmail.com>
2021-09-27 23:51:18 -0700

    Update test.cpp

--- src/projectM-emscripten/test.cpp
+++ src/projectM-emscripten/test.cpp
@@ -33,7 +33,7 @@ static void renderFrame(){
 auto sndBuf=wave.snd+wave.pos;
 auto sndat=reinterpret_cast<short*>(sndBuf);
 app.pm->pcm()->addPCM16Data(sndat,1024);
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT);
 app.pm->renderFrame();
 glFlush();
 SDL_GL_SwapWindow(app.win);

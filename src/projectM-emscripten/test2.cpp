c7d2f92896918f6fe1cb10490092fc8752f63cc6
Noah Cohn <noahc42@gmail.com>
2021-10-01 04:52:36 -0700

    Update test2.cpp

--- src/projectM-emscripten/test2.cpp
+++ src/projectM-emscripten/test2.cpp
@@ -68,7 +68,7 @@ eglInitialize(display,&major,&minor);
 if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
 if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
 }
-EGLint anEglCtxAttribs2[]={EGL_CONTEXT_CLIENT_VERSION,3,EGL_NONE,EGL_NONE};
+EGLint anEglCtxAttribs2[]={EGL_CONTEXT_MAJOR_VERSION,3,EGL_CONTEXT_MINOR_VERSION,2,EGL_CONTEXT_OPENGL_ROBUST_ACCESS,EGL_TRUE,EGL_NONE};
 contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
 if(contextegl==EGL_NO_CONTEXT){
 }

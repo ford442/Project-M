aee7232f1dfd6cf6a87b254c28ded40f65d9bf3d
Noah Cohn <noahc42@gmail.com>
2021-10-02 06:57:21 -0700

    Update lazuli.cpp

--- src/projectM-emscripten/lazuli.cpp
+++ src/projectM-emscripten/lazuli.cpp
@@ -2,24 +2,18 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <GLES3/gl32.h>
 #include <EGL/egl.h>
-
 #include <emscripten/emscripten.h>
 #include <emscripten/html5.h>
-
 #include "SDL2/SDL_config.h"
 #include <SDL2/SDL.h>
-
 #include <projectM.hpp>
-
 Uint8 * stm;
 static const EGLint attribut_list[]={
 EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_SRGB,
 EGL_NONE
 };
-
 static const EGLint attribute_list[]={
 EGL_RED_SIZE,16,
 EGL_GREEN_SIZE,16,
@@ -31,25 +25,21 @@ EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
 EGL_CONFORMANT,EGL_OPENGL_ES3_BIT,
 EGL_NONE
 };
-
 EGLSurface surface;
 EGLDisplay display;
 EGLContext contextegl;
 const float FPS=60;
-
 static SDL_AudioDeviceID dev;
 static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
 typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}
 projectMApp;projectMApp app;
-
 static void renderFrame(){
 auto sndat=reinterpret_cast<short*>(stm);
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 app.pm->pcm()->addPCM16Data(sndat,768);
 app.pm->renderFrame();
 eglSwapBuffers(display,surface);
 }
-
 static void chngt(){
 SDL_GL_SetAttribute(SDL_GL_RED_SIZE,16);
 SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,16);
@@ -63,7 +53,6 @@ SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,16);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,16);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,16);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,16);
-    
 EmscriptenWebGLContextAttributes attr;
 attr.alpha=1;
 attr.stencil=1;
@@ -212,7 +201,46 @@ document.getElementById("contain2").width=window.innerHeight;
 document.getElementById("contain2").height=window.innerHeight;
 document.getElementById('btn3').addEventListener('click',function(){
 window.open('https://test.1ink.us/libflac.js/');
-});});
+document.getElementById('btn2').addEventListener('click',function(){
+document.getElementById("ihig").innerHTML=window.innerHeight;
+document.getElementById("circle").height=window.innerHeight;
+document.getElementById("circle").width=window.innerWidth;
+document.getElementById('btn3').style.border="5px solid green";
+document.getElementById('btn2').style.border="5px solid red";
+document.getElementById('btn2').style.background="red";
+document.getElementById('btn').style.border="5px solid green";
+document.getElementById('btn7').style.border="5px solid green";
+document.getElementById('di').click();
+});
+document.getElementById('btn').addEventListener('click',function(){
+document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn6').style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;"
+});
+document.getElementById('btn6').addEventListener('click',function(){
+let midl=Math.round((window.innerWidth*0.5)-(window.innerHeight*0.5));
+document.getElementById("contain2").style="background-color:rgba(0,0,0,1);pointer-events:none;z-index:999993;height:"+window.innerHeight+"px;width:"+window.innerHeight+"px;position:absolute;top:0px;left:"+midl+"px;right:"+midl+"px;";
+document.getElementById("canvas").style="position:absolute;left:0;top:0;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:pixelated;width:"+window.innerHeight+"px;height:"+window.innerHeight+"px;";
+document.getElementById("drop").style="z-index:999994;height:100vh;width:auto;position:absolute;text-align:center;background-color:rgba(1,1,1,1);";
+document.getElementById('ihig').innerHTML=Math.round(window.innerHeight);
+document.getElementById("circle").height=window.innerHeight;
+document.getElementById("circle").width=window.innerWidth;
+document.getElementById('di').click();
+});
+document.getElementById('btn7').addEventListener('click',function(){
+document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById("drop").style="z-index:999994;height:100vh;width:auto;position:absolute;text-align:center;";
+document.getElementById('btn6').style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
+document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";
+document.getElementById('shut').innerHTML=2;
+document.getElementById('di').click();
+});});});
 app.done=0;
 return 1;
 }

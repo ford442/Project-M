b9f0eba328146dd5b7001fb971c5016ad9124a71
Noah Cohn <noahc42@gmail.com>
2021-10-06 18:37:47 -0700

    Update b3filesys.js

--- src/projectM-emscripten/b3filesys.js
+++ src/projectM-emscripten/b3filesys.js
@@ -33,7 +33,6 @@ window.open('https://test.1ink.us/libflac.js/');
 });
 
 document.getElementById('btn2').addEventListener('click',function(){
-document.getElementById('btn6').click();
 let pth=document.getElementById('path').innerHTML;
 let pth2=document.getElementById('path2').innerHTML;
 let pth3=document.getElementById('path3').innerHTML;
@@ -109,7 +108,7 @@ document.getElementById('btn7').addEventListener('click',function(){
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn5').style="background-color:pink;position:absolute;display:block;left:3%;top:43%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn3').style="background-color:red;position:absolute;display:block;left:3%;top:13%;z-index:999997;border:5px solid red;border-radius:50%;";
-document.getElementById("cvanvas").style="background-color:rgba(1,1,1,0);z-index:999994;height:100vh;width:auto;";
+document.getElementById("vcanvas").style="background-color:rgba(1,1,1,0);z-index:999994;height:100vh;width:auto;";
 document.getElementById('btn6').style="background-color:yellow;position:absolute;display:block;left:3%;top:53%;z-index:999997;border:5px solid green;border-radius:50%;";
 document.getElementById('btn').style="background-color:red;position:absolute;display:block;left:3%;top:23%;z-index:999997;border:5px solid red;border-radius:50%;";
 document.getElementById('btn7').style="background-color:red;position:absolute;display:block;left:3%;top:63%;z-index:999997;border:5px solid red;border-radius:50%;";

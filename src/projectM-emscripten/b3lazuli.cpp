219567bc2d37710ae405c17dc9f45b6d9c71c14e
Noah Cohn <noahc42@gmail.com>
2021-10-06 18:29:15 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -10,38 +10,29 @@
 #include "SDL2/SDL_config.h"
 #include <SDL2/SDL.h>
 #include <projectM.hpp>
-
-Uint8 *stm;
-
+Uint8* stm;
 static const EGLint attribut_list[]={
 EGL_GL_COLORSPACE,EGL_GL_COLORSPACE_SRGB,
 EGL_NONE
 };
-
 static const EGLint attribute_list[]={
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 EGL_RED_SIZE,32,
 EGL_GREEN_SIZE,32,
 EGL_BLUE_SIZE,32,
 EGL_ALPHA_SIZE,32,
-EGL_ALPHA_MASK_SIZE,32,
 EGL_STENCIL_SIZE,32,
 EGL_DEPTH_SIZE,32,
 EGL_NONE
 };
-
 EGLSurface surface;
 EGLDisplay display;
 EGLContext contextegl;
 const float FPS=60;
 static SDL_AudioDeviceID dev;
 static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
-typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;
-bool done;projectM::Settings settings;SDL_AudioDeviceID dev;
-}projectMApp;
-
-projectMApp app;
-
+typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}
+projectMApp;projectMApp app;
 static void renderFrame(){
 auto sndat=reinterpret_cast<short*>(stm);
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
@@ -49,11 +40,7 @@ app.pm->pcm()->addPCM16Data(sndat,768);
 app.pm->renderFrame();
 eglSwapBuffers(display,surface);
 }
-
 static void chngt(){
-
-int width=EM_ASM_INT({return document.getElementById('ihig').innerHTML;});
-int height=width;
 SDL_GL_SetAttribute(SDL_GL_RED_SIZE,32);
 SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,32);
 SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,32);
@@ -65,8 +52,6 @@ SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,32);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,32);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,32);
 SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,32);
-app.win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
-
 EmscriptenWebGLContextAttributes attr;
 attr.alpha=1;
 attr.stencil=1;
@@ -77,32 +62,32 @@ attr.preserveDrawingBuffer=0;
 emscripten_webgl_init_context_attributes(&attr);
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#canvas",&attr);
 emscripten_webgl_make_context_current(ctx);
-
 EGLConfig eglconfig=NULL;
 EGLint config_size,major,minor;
-display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
+EGLDisplay display=eglGetDisplay(EGL_DEFAULT_DISPLAY);
 eglInitialize(display,&major,&minor);
 if(eglChooseConfig(display,attribute_list,&eglconfig,1,&config_size)==EGL_TRUE && eglconfig!=NULL){
 if(eglBindAPI(EGL_OPENGL_ES_API)!=EGL_TRUE){
 }
+
 EGLint anEglCtxAttribs2[]={
 EGL_CONTEXT_CLIENT_VERSION,3,
 EGL_NONE};
+
 contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
 if(contextegl==EGL_NO_CONTEXT){
 }
 else{
-surface=eglCreateWindowSurface(display,eglconfig,NULL,attribut_list);
+EGLSurface surface=eglCreateWindowSurface(display,eglconfig,NULL,attribut_list);
 eglMakeCurrent(display,surface,surface,contextegl);
-}}  
-
+}}
+int width=EM_ASM_INT({return document.getElementById('ihig').innerHTML;});
+int height=width;
+app.win=SDL_CreateWindow("pm",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL);
 app.glCtx=&contextegl;
-  
 SDL_SetWindowTitle(app.win,"1ink.us - Lazuli");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-  
-
 app.settings.meshX=128;
 app.settings.meshY=128;
 app.settings.textureSize=2048;
@@ -111,20 +96,18 @@ app.settings.windowWidth=width;
 app.settings.windowHeight=width;
 app.settings.smoothPresetDuration=22;
 app.settings.presetDuration=88;
-app.settings.beatSensitivity=1.0;
+app.settings.beatSensitivity=0.3;
 app.settings.aspectCorrection=0;
 app.settings.easterEgg=0;
 app.settings.shuffleEnabled=0;
 app.settings.softCutRatingsEnabled=1;
-app.settings.presetURL="/presets";
-  
+app.settings.presetURL="/presets";  
 app.pm=new projectM(app.settings);
 printf("Init ProjectM\n");
-app.pm->selectRandom(true);
-printf("Select random preset.\n");
 app.pm->projectM_resetGL(width,height);
 printf("Reseting GL.\n");
-
+app.pm->selectRandom(true);
+printf("Select random preset.\n");
 DIR *m_dir;
 if((m_dir=opendir("/"))==NULL){printf("error opening /\n");
 }
@@ -136,11 +119,9 @@ printf("%s\n",dir_entry->d_name);
 for(uint i=0;i<app.pm->getPlaylistSize();i++){
 printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
-
 glClearColor(1.0,1.0,1.0,0.0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
-
 void swtcht(){
 app.pm->selectRandom(true);
 printf("Select random preset.\n");
@@ -167,7 +148,7 @@ qu(2);
 }
 SDL_PauseAudioDevice(dev,SDL_FALSE);
 }
-static void SDLCALL bfr(void *unused,Uint8 *stm,int len){
+static void SDLCALL bfr(void *unused,Uint8* stm,int len){
 Uint8 *wptr;
 int lft;
 wptr=wave.snd+wave.pos;
@@ -217,7 +198,7 @@ swtcht();
 EM_JS(void,ma,(),{
 let d=S();if(d)d();d=S();
 function S(){
-let canvas=document.getElementById("vcanvas");let g=new GPU({canvas:canvas,mode:'gpu'});let w=document.getElementById("iwid").innerHTML;
+let canvas=document.getElementById("vcanvas");let g=new GPU({canvas:{canvas}});let w=document.getElementById("iwid").innerHTML;
 let h=document.getElementById("ihig").innerHTML;w=Math.round(w);h=Math.round(h);
 let Rn=document.getElementById("frate").innerHTML;let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);
 let W=new WebAssembly.Memory({initial:m});let o=[w,h];

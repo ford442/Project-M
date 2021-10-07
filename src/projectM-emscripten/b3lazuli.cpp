4cba4a543d288b850fe6cac1644cbbf16c0c07b8
Noah Cohn <noahc42@gmail.com>
2021-10-07 02:16:13 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -192,22 +192,8 @@ void swtch(){
 swtcht();
 }}
 
-EM_JS(void,ma,(),{
-let d=S();if(d)d();d=S();
-function S(){
-let yy=document.getElementById("cp");let canvas=yy.querySelector("canvas");let g=new GPU({canvas:{canvas},mode:'gpu'});let w=document.getElementById("iwid").innerHTML;
-let h=document.getElementById("ihig").innerHTML;w=Math.round(w);h=Math.round(h);
-let Rn=document.getElementById("frate").innerHTML;let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);
-let W=new WebAssembly.Memory({initial:m});let o=[w,h];
-let v=document.getElementById("mv");let t=g.createKernel(function(v){let p=v[this.thread.y][this.thread.x];
-return[p[0],p[1],p[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);
-let r=g.createKernel(function(f){let p=f[this.thread.y][this.thread.x];
-this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);
-let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));
-$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);canvas=yy.querySelector("canvas");function M(){if(T){return;}r(t($));$.set(t(v),0);
-let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();
-document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();
-S();};return()=>{T=true;};}});
+EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=document.getElementById("iwid").innerHTML;let h=document.getElementById("ihig").innerHTML;w=Math.round(w);h=Math.round(h);const g=new GPU();let Rn=document.getElementById("frate").innerHTML;let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];let ki=document.getElementById("cp");let canvas=ki.querySelector("canvas");const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);
+let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);canvas=ki.querySelector("canvas");function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let kt=Math.floor(mq);let y=((kt*f)-(kt*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
 EM_ASM({
 FS.mkdir('/presets');

d73b1f514ffb33f81383c5cbd34a28c1a4d368f8
Noah Cohn <noahc42@gmail.com>
2021-10-08 16:40:26 -0700

    Update ccall.js

--- src/projectM-emscripten/ccall.js
+++ src/projectM-emscripten/ccall.js
@@ -9,7 +9,7 @@ Module.ccall('chng');
 });
 document.getElementById('btn7').addEventListener('click',function(){
 document.getElementById("contain2").style="z-index:999993;height:1440px;width:1440px;position:absolute;top:0px;left:0px;";
-document.getElementById("canvas").style="position:absolute;left:0;top:0;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:pixelated;width:1440px;height:1440px;";
+document.getElementById("canvas").style="position:absolute;left:0px;top:0px;background-color:rgba(1,1,1,0);z-index:999995;image-rendering:pixelated;width:1440px;height:1440px;";
 document.getElementById('ihig').innerHTML=1440;
 document.getElementById('iwid').innerHTML=1440;
 document.getElementById('btn4').style="background-color:grey;position:absolute;display:block;left:3%;top:33%;z-index:999997;border:5px solid green;border-radius:50%;";

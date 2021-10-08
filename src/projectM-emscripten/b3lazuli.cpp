829864b1403f34e51d58771359ae6de5085d6d3b
Noah Cohn <noahc42@gmail.com>
2021-10-08 16:39:38 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -37,7 +37,8 @@ EGL_RED_SIZE,32,
 EGL_GREEN_SIZE,32,
 EGL_BLUE_SIZE,32,
 EGL_ALPHA_SIZE,32,
-EGL_STENCIL_SIZE,32,
+EGL_ALPHA_MASK_SIZE,32,
+EGL_STENCIL_SIZE,8,
 EGL_DEPTH_SIZE,32,
 EGL_NONE
 };
@@ -85,15 +86,15 @@ app.glCtx=&contextegl;
 SDL_SetWindowTitle(app.win,"1ink.us - Lazuli");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-app.settings.meshX=128;
-app.settings.meshY=128;
+app.settings.meshX=90;
+app.settings.meshY=90;
 app.settings.textureSize=2048;
 app.settings.fps=FPS;
 app.settings.windowWidth=width;
 app.settings.windowHeight=width;
 app.settings.smoothPresetDuration=22;
 app.settings.presetDuration=88;
-app.settings.beatSensitivity=1.0;
+app.settings.beatSensitivity=2.0;
 app.settings.aspectCorrection=0;
 app.settings.easterEgg=0;
 app.settings.shuffleEnabled=0;
@@ -191,17 +192,7 @@ lckt();
 void swtch(){
 swtcht();
 }}
-EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=document.getElementById("iwid").innerHTML;
-   let h=document.getElementById("ihig").innerHTML; w=Math.round(w);h=Math.round(h);
-      const g=new GPU();let Rn=document.getElementById("frate").innerHTML;
-     let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});
-   let o=[w,h];const v=document.getElementById("mv");
-    const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);
-let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));
-  $.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);let canvas=document.querySelector("canvas");
-  function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let kt=Math.floor(mq);let y=((kt*f)-(kt*Rn));
- if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;
-   t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
+EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML);let h=parseInt(document.getElementById("ihig").innerHTML);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled: false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
 EM_ASM({
 FS.mkdir('/presets');

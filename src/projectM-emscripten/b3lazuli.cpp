cd946e3b8d38fca9c7ca80cdcc783d789208fcea
Noah Cohn <noahc42@gmail.com>
2021-10-10 16:12:11 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -16,7 +16,7 @@ EGLContext contextegl;
 EGLSurface surface;
 EmscriptenWebGLContextAttributes attr;
 Uint8* stm;
-const float FPS=60;
+const float FPS=120;
 static SDL_AudioDeviceID dev;
 
 static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
@@ -28,7 +28,7 @@ static void renderFrame(){
 auto sndat=reinterpret_cast<short*>(stm);
 glClearColor(1.0,1.0,1.0,0.0);
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
-app.pm->pcm()->addPCM16Data(sndat,768);
+app.pm->pcm()->addPCM16Data(sndat,1024);
 app.pm->renderFrame();
 eglSwapBuffers(display,surface);
 }
@@ -41,7 +41,7 @@ EGL_NONE
 static const EGLint attribute_list[]={
 EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 EGL_TRANSPARENT_TYPE,EGL_TRANSPARENT_RGB,
-EGL_BUFFER_SIZE,128,
+EGL_BUFFER_SIZE,32,
 EGL_RED_SIZE,32,
 EGL_GREEN_SIZE,32,
 EGL_BLUE_SIZE,32,
@@ -66,7 +66,7 @@ attr.stencil=1;
 attr.depth=1;
 attr.antialias=0;
 attr.premultipliedAlpha=0;
-attr.preserveDrawingBuffer=1;
+attr.preserveDrawingBuffer=0;
 emscripten_webgl_init_context_attributes(&attr);
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#canvas",&attr);
 EGLConfig eglconfig=NULL;
@@ -95,9 +95,9 @@ app.glCtx=&contextegl;
 SDL_SetWindowTitle(app.win,"1ink.us - Lazuli");
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
-app.settings.meshX=64;
-app.settings.meshY=64;
-app.settings.textureSize=1024;
+app.settings.meshX=100;
+app.settings.meshY=100;
+app.settings.textureSize=4096;
 app.settings.fps=FPS;
 app.settings.windowWidth=width;
 app.settings.windowHeight=width;
@@ -201,7 +201,7 @@ lckt();
 void swtch(){
 swtcht();
 }}
-EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML);let h=parseInt(document.getElementById("ihig").innerHTML);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:true,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled: false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
+EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML,10);let h=parseInt(document.getElementById("ihig").innerHTML,10);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled: false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
 EM_ASM({
 FS.mkdir('/presets');

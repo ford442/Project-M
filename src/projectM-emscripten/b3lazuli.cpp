1ab66de59ffcd99da27e7b951cb8344313281b9f
Noah Cohn <noahc42@gmail.com>
2021-10-11 16:50:01 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -51,13 +51,6 @@ EGL_STENCIL_SIZE,64,
 EGL_DEPTH_SIZE,64,
 EGL_NONE
 };
-SDL_GL_SetAttribute(SDL_GL_RED_SIZE,8);
-SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,8);
-SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,8);
-SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,32);
-SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
-SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,8);
-SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE,8);
 attr.alpha=1;
 attr.stencil=1;
 attr.depth=1;

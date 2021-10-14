00ed7ff721249834088c68ab91f89a5e877f5b33
Noah Cohn <noahc42@gmail.com>
2021-10-13 21:43:05 -0700

    Update index.js

--- src/projectM-emscripten/index.js
+++ src/projectM-emscripten/index.js
@@ -1,22 +1,9 @@
 var MyModule =
 {
-    canvas: (function() {
-    var aCanvas = document.getElementById('canvas');
+canvas: (function() {
+var aCanvas = document.getElementById('canvas');
   //  aGlCtx = aCanvas.getContext ('webgl2',  { alpha: true, premultipliedAlpha: false, depth: false, antialias: false, preserveDrawingBuffer: false } ); 
-    return aCanvas;
-  })(),
-    preRun: (function() {
-FS.mkdir('/presets');
-FS.mkdir('/snd');
-FS.mkdir('/textures');
-var id = FS.makedev(64, 0);
-FS.registerDevice(id, {});
-FS.mkdev('/presets', id);
-var id2 = FS.makedev(128, 0);
-FS.registerDevice(id2, {});
-FS.mkdev('/snd', id2);
+return aCanvas;
 })(),
-
 };
-
 const OccViewerModuleInitialized = createModule(MyModule);

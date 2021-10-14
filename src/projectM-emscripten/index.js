49a678699c7741c30696caa5d3d89cc8e206b482
Noah Cohn <noahc42@gmail.com>
2021-10-13 21:23:41 -0700

    Update index.js

--- src/projectM-emscripten/index.js
+++ src/projectM-emscripten/index.js
@@ -5,6 +5,17 @@ var MyModule =
   //  aGlCtx = aCanvas.getContext ('webgl2',  { alpha: true, premultipliedAlpha: false, depth: false, antialias: false, preserveDrawingBuffer: false } ); 
     return aCanvas;
   })(),
+    preRun: (function() {
+FS.mkdir('/presets');
+FS.mkdir('/snd');
+FS.mkdir('/textures');
+var id = FS.makedev(64, 0);
+FS.registerDevice(id, {});
+FS.mkdev('/presets', id);
+var id2 = FS.makedev(128, 0);
+FS.registerDevice(id2, {});
+FS.mkdev('/snd', id2);
+})(),
 
 };
 

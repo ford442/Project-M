3688381491dd3d74edc5136e2f9c3f610e2f6b39
Noah Cohn <noahc42@gmail.com>
2021-10-15 07:18:21 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -93,7 +93,7 @@ app.settings.beatSensitivity=7.0;
 app.settings.aspectCorrection=0;
 app.settings.easterEgg=0;
 app.settings.shuffleEnabled=0;
-app.settings.hardcutEnabled=false;
+app.settings.hardcutEnabled=true;
 app.settings.softCutRatingsEnabled=1;
 app.settings.presetURL="/presets";  
 app.pm=new projectM(app.settings);
@@ -116,11 +116,11 @@ printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 glClearColor(1.0,1.0,1.0,0.0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
 }
-void swtcht(){
+static void swtcht(){
 app.pm->selectRandom(true);
 printf("Select random preset.\n");
 }
-void lckt(){
+static void lckt(){
 app.pm->setPresetLock(true);
 printf("Preset locked.\n");
 }
@@ -176,19 +176,19 @@ wave.spec.callback=bfr;
 opn_aud();
 }
 extern "C" {
-void pl(){
+static void pl(){
 plt();
 }
-void chng(){
+static void chng(){
 chngt();
 }
-void lck(){
+static void lck(){
 lckt();
 }
-void swtch(){
+static void swtch(){
 swtcht();
 }}
-EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML,10);let h=parseInt(document.getElementById("ihig").innerHTML,10);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
+EM_JS(static void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML,10);let h=parseInt(document.getElementById("ihig").innerHTML,10);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
 EM_ASM({
 FS.mkdir('/snd');

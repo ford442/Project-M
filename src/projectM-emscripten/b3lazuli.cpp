4944884e380eb9913021d7b5f1e7ad35397e7e0c
Noah Cohn <noahc42@gmail.com>
2021-10-18 01:51:59 -0700

    Update b3lazuli.cpp

--- src/projectM-emscripten/b3lazuli.cpp
+++ src/projectM-emscripten/b3lazuli.cpp
@@ -11,27 +11,14 @@
 #include "SDL2/SDL_config.h"
 #include <projectM.hpp>
 
-EGLDisplay display;
-EGLContext contextegl;
-EGLSurface surface;
-EmscriptenWebGLContextAttributes attr;
-Uint8 *stm;
+static EGLDisplay display;
+static EGLContext contextegl;
+static EGLSurface surface;
+static EmscriptenWebGLContextAttributes attr;
+Uint8* stm;
 const float FPS=30;
 static SDL_AudioDeviceID dev;
-static SDL_AudioSpec spec;
-
-static struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
-
-static void SDLCALL bfr(void *unused,Uint8 *stm,int len);
-
-spec.freq = 44100;
-spec.format = AUDIO_U16;
-spec.channels = 2;
-spec.samples = 512;
-spec.callback = bfr;
-spec.userdata = NULL;
-
-
+static struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
 typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}
 projectMApp;projectMApp app;
 
@@ -152,8 +139,8 @@ qu(2);
 }
 SDL_PauseAudioDevice(dev,SDL_FALSE);
 }
-static void SDLCALL bfr(void *unused,Uint8 *stm,int len){
-Uint8 *wptr;
+static void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
 int lft;
 wptr=wave.snd+wave.pos;
 lft=wave.slen-wave.pos;
@@ -200,12 +187,12 @@ swtcht();
 }}
 EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w=parseInt(document.getElementById("iwid").innerHTML,10);let h=parseInt(document.getElementById("ihig").innerHTML,10);w=Math.round(w);h=Math.round(h);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w*h*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w,h];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
+ma();
 EM_ASM({
 FS.mkdir('/snd');
 FS.mkdir('/textures');
 FS.mkdir('/presets');
 });
 app.done=0;
-ma();
 return 1;
 }

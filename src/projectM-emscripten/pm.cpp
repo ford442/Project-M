f22b0b46ae99f08bec2e978e5fa8d68b768fbb43
Noah Cohn <noahc42@gmail.com>
2021-10-29 03:39:04 -0700

    Update pm.cpp

--- src/projectM-emscripten/pm.cpp
+++ src/projectM-emscripten/pm.cpp
@@ -10,9 +10,6 @@
 #include <SDL2/SDL.h>
 #include "SDL2/SDL_config.h"
 #include <projectM.hpp>
-#include <vector>
-#include <wasm_simd128.h>
-#include <xmmintrin.h>
 
 Uint8 * stm;
 static EGLDisplay display;
@@ -26,8 +23,6 @@ typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;proje
 projectMApp;projectMApp app;
 
 static void rFrame(){
-glClearColor( 1.0, 1.0, 1.0, 0.0 );
-glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 app.pm->renderFrame();
 eglSwapBuffers(display,surface);
 auto sndat=reinterpret_cast<short*>(stm);
@@ -85,7 +80,7 @@ SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
 app.settings.meshX=96;
 app.settings.meshY=96;
-app.settings.textureSize=1024;
+app.settings.textureSize=2048;
 app.settings.fps=FPS;
 app.settings.windowWidth=width;
 app.settings.windowHeight=width;
@@ -114,9 +109,6 @@ printf("%s\n",dir_entry->d_name);
 for(uint i=0;i<app.pm->getPlaylistSize();i++){
 printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 }
-glClearColor(1.0,1.0,1.0,0.0);
-auto sndat=reinterpret_cast<short*>(stm);
-app.pm->pcm()->addPCM16Data(sndat,1024);
 emscripten_set_main_loop((void (*)())rFrame,0,0);
 }
 static void swtcht(){
@@ -194,12 +186,12 @@ swtcht();
 EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w$=parseInt(document.getElementById("iwid").innerHTML,10);let h$=parseInt(document.getElementById("ihig").innerHTML,10);w$=Math.round(w$);h$=Math.round(h$);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 }
 int main(){
-ma();
 EM_ASM({
 FS.mkdir('/snd');
 FS.mkdir('/textures');
 FS.mkdir('/presets');
 });
 app.done=0;
+ma();
 return 1;
 }

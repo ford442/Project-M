046624cd897597cdfb9fe822b22d0f9198f14e91
Noah Cohn <noahc42@gmail.com>
2021-12-08 03:34:36 -0800

    Update pm.cpp

--- src/projectM-emscripten/pm.cpp
+++ src/projectM-emscripten/pm.cpp
@@ -4,59 +4,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <time.h>
 #include <GLES3/gl3.h>
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include <SDL2/SDL.h>
-#include "SDL2/SDL_config.h"
 #include <projectM.hpp>
+#include <float.h>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdbool.h>
 
-Uint8 * stm;
-static EGLDisplay display;
-static EGLContext contextegl;
-static EGLSurface surface;
-static EmscriptenWebGLContextAttributes attr;
-const float FPS=60.0;
-static SDL_AudioDeviceID dev;
-static struct{SDL_AudioSpec spec;Uint8* snd;Uint32 slen;int pos;}wave;
+
+Uint8 *stm;
+EGLDisplay display;
+EGLContext contextegl;
+EGLSurface surface;
+EmscriptenWebGLContextAttributes attr;
+static const float FPS=60.0;
+SDL_AudioDeviceID dev;
+struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
 typedef struct{projectM *pm;SDL_Window *win;SDL_GLContext *glCtx;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}
 projectMApp;projectMApp app;
 
 static void renderFrame(){
 app.pm->renderFrame();
 eglSwapBuffers(display,surface);
-auto sndat=reinterpret_cast<short*>(stm);
+auto sndat=reinterpret_cast<short*>(&stm);
 app.pm->pcm()->addPCM16Data(sndat,1024);
 }
 static const EGLint attribut_list[]={
-// EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
+EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 EGL_NONE
 };
 static const EGLint attribute_list[]={
-// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
-EGL_RED_SIZE,8,
-EGL_GREEN_SIZE,8,
-EGL_BLUE_SIZE,8,
-EGL_ALPHA_SIZE,8,
-EGL_STENCIL_SIZE,8,
-EGL_DEPTH_SIZE,24,
+EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
+EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
+EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+EGL_RED_SIZE,32,
+EGL_GREEN_SIZE,32,
+EGL_BLUE_SIZE,32,
+EGL_ALPHA_SIZE,32,
+EGL_STENCIL_SIZE,32,
+EGL_DEPTH_SIZE,32,
+EGL_BUFFER_SIZE,64,
 EGL_NONE
 };
 EGLint anEglCtxAttribs2[]={
 EGL_CONTEXT_CLIENT_VERSION,3,
-// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 EGL_NONE
 };
 static void chngt(){
-SDL_GL_SetAttribute( SDL_GL_RED_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_ACCUM_GREEN_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_ACCUM_BLUE_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE,8);
-SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,16);
-SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,1);
+SDL_GL_SetAttribute(SDL_GL_RED_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ACCUM_RED_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ACCUM_GREEN_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ACCUM_BLUE_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ACCUM_ALPHA_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,32);
+SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
+SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE,1);
+SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
 attr.alpha=1;
 attr.stencil=1;
 attr.depth=1;
@@ -145,7 +162,7 @@ qu(2);
 }
 SDL_PauseAudioDevice(dev,SDL_FALSE);
 }
-static void SDLCALL bfr(void *unused,Uint8 * stm,int len){
+static void SDLCALL bfr(void *unused,Uint8 *stm,int len){
 Uint8 *wptr;
 int lft;
 wptr=wave.snd+wave.pos;
@@ -163,7 +180,7 @@ wave.pos+=len;
 }
 static void plt(){
 cls_aud();
-char flnm[1024];
+char flnm[16];
 SDL_FreeWAV(wave.snd);
 SDL_Quit();
 SDL_SetMainReady();
@@ -190,8 +207,9 @@ lckt();
 }
 void swtch(){
 swtcht();
-}}
+}
 EM_JS(void,ma,(),{let d=S();if(d)d();d=S();function S(){let w$=parseInt(document.getElementById("iwid").innerHTML,10);let h$=parseInt(document.getElementById("ihig").innerHTML,10);w$=Math.round(w$);h$=Math.round(h$);let canvas=document.getElementById("vcanvas");let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,premultipliedAlpha:false,imageSmoothingEnabled:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2});const g=new GPU({canvas:canvas,webGl:contx});let Rn=parseInt(document.getElementById("frate").innerHTML,10);let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];const v=document.getElementById("mv");const t=g.createKernel(function(v){const P=v[this.thread.y][this.thread.x];return[P[0],P[1],P[2]];}).setTactic("precision").setPipeline(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);const r=g.createKernel(function(f){const p=f[this.thread.y][this.thread.x];this.color(p[0],p[1],p[2]);}).setTactic("precision").setGraphical(true).setOutput(o).setStrictIntegers(true).setFixIntegerDivisionAccuracy(false);let $=new Uint8ClampedArray(W.buffer,0,l);$.set(t(v),0);r(t($));$.set(t(v),0);r(t($));$.set(t(v),0);let T=false;let ms=1;let R=16;let f=(1000/Rn);function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.floor(mq);let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
+}
 int main(){
 EM_ASM({
 FS.mkdir('/snd');

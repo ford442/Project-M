75114fff24814b7e707cfd807ef254d9624cec78
Noah Cohn <noahc42@gmail.com>
2022-01-29 18:40:29 -0800

    Update pm.cpp

--- src/projectM-emscripten/pm.cpp
+++ src/projectM-emscripten/pm.cpp
@@ -56,12 +56,12 @@ EGL_NONE
 
 static const EGLint attribute_list[]={
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
- EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
+EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
 EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
- EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
 // EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
 // EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
- EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
+EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
 EGL_RED_SIZE,8,
 EGL_GREEN_SIZE,8,
 EGL_BLUE_SIZE,8,
@@ -101,7 +101,7 @@ contextegl=eglCreateContext(display,eglconfig,EGL_NO_CONTEXT,anEglCtxAttribs2);
 surface=eglCreateWindowSurface(display,eglconfig,NULL,attribut_list);
 eglMakeCurrent(display,surface,surface,contextegl);
 emscripten_webgl_make_context_current(ctx);
-int width=EM_ASM_INT({return parseInt(document.getElementById('pmhig').innerHTML,10);});
+int width=EM_ASM_INT({return document.getElementById('pmhig').innerHTML;});
 int height=width;
 app.settings.meshX=60;
 app.settings.meshY=60;
@@ -162,7 +162,6 @@ qu(2);
 }
 SDL_PauseAudioDevice(dev,SDL_FALSE);
 }
-
 static void SDLCALL bfr(void *unused,Uint8 *stm,int len){
 wptr=wave.snd+wave.pos;
 lft=wave.slen-wave.pos;
@@ -177,7 +176,6 @@ wave.pos=0;
 SDL_memcpy(stm,wptr,len);
 wave.pos+=len;
 }
-
 static void plt(){
 cls_aud();
 SDL_FreeWAV(wave.snd);
@@ -195,26 +193,27 @@ wave.spec.callback=bfr;
 opn_aud();
 }
 extern "C" {
- void pl(){
+void pl(){
 plt();
 }
- void chng(){
+void chng(){
 chngt();
 }
- void lck(){
+void lck(){
 lckt();
 }
- void swtch(){
+void swtch(){
 swtcht();
 }}
- EM_JS(void,ma,(),{
+EM_JS(void,ma,(),{
 let d=S();if(d)d();d=S();function S(){
-let w$=parseInt(document.getElementById("iwid").innerHTML,10);
-let h$=parseInt(document.getElementById("ihig").innerHTML,10);
-w$=Math.round(w$);h$=Math.round(h$);let canvas=document.getElementById("vcanvas");
-let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,desynchronized:false});
-const g=new GPU({canvas:canvas,webGl:contx});
-let Rn=parseInt(document.getElementById("frate").innerHTML,10);
+let w$=document.getElementById("iwid").innerHTML;
+let h$=document.getElementById("ihig").innerHTML;
+w$=Math.round(w$);h$=Math.round(h$);
+// let canvas=document.getElementById("vcanvas");
+// let contx=canvas.getContext('webgl2',{alpha:false,stencil:false,depth:false,preserveDrawingBuffer:false,premultipliedAlpha:false,lowLatency:true,powerPreference:'high-performance',majorVersion:2,desynchronized:false});
+const g=new GPU();
+let Rn=document.getElementById("frate").innerHTML;
 let l=(w$*h$*4);let m=((l/65536)+1);m=Math.floor(m);
 let W=new WebAssembly.Memory({initial:m});let o=[w$,h$];
 const v=document.getElementById("mv");
@@ -228,7 +227,7 @@ function M(){if(T){return;}r(t($));$.set(t(v),0);let mq=((ms*f)/R);let k=Math.fl
 let y=((k*f)-(k*Rn));if(y>8){R=8;}ms=ms+1;setTimeout(function(){M();},R);}M();
 document.getElementById("di").onclick=function(){T=true;t.destroy();r.destroy();g.destroy();S();};return()=>{T=true;};}});
 int main(){
- EM_ASM({
+EM_ASM({
 FS.mkdir('/snd');
 FS.mkdir('/textures');
 FS.mkdir('/presets');

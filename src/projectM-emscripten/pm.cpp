90a29c6b58451aa7cdc95005b5c7aa1e600850a3
Noah Cohn <noahc42@gmail.com>
2022-01-29 17:40:35 -0800

    Update pm.cpp

--- src/projectM-emscripten/pm.cpp
+++ src/projectM-emscripten/pm.cpp
@@ -1,7 +1,8 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include <GLES3/gl3.h>
-#include <GLES3/gl3ext.h>
+#define __gl2_h_
+#include <GLES2/gl2ext.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include "SDL2/SDL_config.h"

fba541cb783e45450378e6a096d963fd1a7dc8a6
Noah Cohn <noahc42@gmail.com>
2022-02-15 04:15:41 -0800

    Update GLSLGenerator.cpp

--- src/libprojectM/Renderer/hlslparser/src/GLSLGenerator.cpp
+++ src/libprojectM/Renderer/hlslparser/src/GLSLGenerator.cpp
@@ -235,8 +235,8 @@ bool GLSLGenerator::Generate(HLSLTree* tree, Target target, Version version, con
     else if (m_version == Version_300_ES)
     {
         m_writer.WriteLine(0, "#version 300 es");
-        m_writer.WriteLine(0, "precision highp float;");
-        m_writer.WriteLine(0, "precision highp sampler3D;");
+        m_writer.WriteLine(0, "precision mediump float;");
+        m_writer.WriteLine(0, "precision mediump sampler3D;");
     }
     else
     {

31e7fd4691355d2a74fbfbf1489815df9291268d
Noah Cohn <noahc42@gmail.com>
2022-02-28 11:13:18 -0800

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,11 @@ set -e # Drop-out from execution if error occurs
 
 printf 'Running autoreconf...'
 
+libtoolize
+aclocal
+automake --add-missing
+autoconf
+
 autoreconf --install
 
 printf '\nSuccess!\nNow please run: emconfigure ./configure\n'

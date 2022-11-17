9d18f3476d98236511708d6ccf6b62c8dbb45cce
Noah Cohn <noahc42@gmail.com>
2022-11-17 10:10:59 -0800

    Update october.cpp

--- src/projectM-emscripten/october.cpp
+++ src/projectM-emscripten/october.cpp
@@ -34,25 +34,18 @@ using namespace std;
 using namespace std::chrono;
 struct timespec rem;
 struct timespec req={0,1500000000};
-EmscriptenWebGLContextAttributes attr;
 
 #define FLAG_DISABLE_PLAYLIST_LOAD 1
 
-EGLDisplay display;
-EGLContext contextegl;
-EGLSurface surface;
-const float FPS=60.0;
 SDL_AudioDeviceID dev;
 struct{SDL_AudioSpec spec;Uint8 *snd;Uint32 slen;int pos;}wave;
+Uint8* stm;
 
 typedef struct{projectM *pm;bool done;projectM::Settings settings;SDL_AudioDeviceID dev;}projectMApp;
 projectMApp app;
-EGLint config_size,major,minor;
-static char flnm[16];
+
 int v0=0,v1=1,v2=2,v3=3,v4=4,v6=6,v8=8,v10=10,v16=16,v24=24,v32=32;
   
-Uint8* stm;
-
 void renderFrame(){
 glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 app.pm->renderFrame();
@@ -62,6 +55,23 @@ app.pm->pcm()->addPCM16Data(sndat,1024/sizeof(short));
 glFinish();
 }
 
+void chngt(){
+static char flnm[16];
+const float FPS=60.0;
+EmscriptenWebGLContextAttributes attr;
+EGLDisplay display;
+EGLContext contextegl;
+EGLSurface surface;
+EGLint config_size,major,minor;
+static EGLint anEglCtxAttribs2[]={
+EGL_CONTEXT_CLIENT_VERSION,v3,
+EGL_CONTEXT_MINOR_VERSION_KHR,v0,
+// EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
+// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
+// EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
+EGL_NONE};
+  
 const EGLint attribut_list[]={
 // EGL_GL_COLORSPACE_KHR,EGL_GL_COLORSPACE_SRGB_KHR,
 EGL_NONE
@@ -70,11 +80,11 @@ EGL_NONE
 const EGLint attribute_list[]={
 // EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
 // EGL_CONTEXT_OPENGL_PROFILE_MASK_KHR,EGL_CONTEXT_OPENGL_COMPATIBILITY_PROFILE_BIT_KHR,
-EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
-EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
-EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
-EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
-EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
+  // EGL_RENDERABLE_TYPE,EGL_OPENGL_ES3_BIT,
+// EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT,EGL_TRUE,
+// EGL_DEPTH_ENCODING_NV,EGL_DEPTH_ENCODING_NONLINEAR_NV,
+// EGL_RENDER_BUFFER,EGL_QUADRUPLE_BUFFER_NV,
+// EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE,EGL_TRUE,
 EGL_RED_SIZE,v8,
 EGL_GREEN_SIZE,v8,
 EGL_BLUE_SIZE,v8,
@@ -86,17 +96,7 @@ EGL_SAMPLE_BUFFERS,v1,
 EGL_SAMPLES,v4,
 EGL_NONE
 };
-
-static EGLint anEglCtxAttribs2[]={
-EGL_CONTEXT_CLIENT_VERSION,v3,
-EGL_CONTEXT_MINOR_VERSION_KHR,v0,
-EGL_CONTEXT_PRIORITY_LEVEL_IMG,EGL_CONTEXT_PRIORITY_REALTIME_NV,
-// EGL_COLOR_COMPONENT_TYPE_EXT,EGL_COLOR_COMPONENT_TYPE_FLOAT_EXT,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_FORWARD_COMPATIBLE_BIT_KHR,
-EGL_CONTEXT_FLAGS_KHR,EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR,
-EGL_NONE};
-
-void chngt(){
+  
 attr.alpha=EM_TRUE;
 attr.stencil=EM_TRUE;
 attr.depth=EM_TRUE;
@@ -108,8 +108,8 @@ attr.powerPreference=EM_WEBGL_POWER_PREFERENCE_HIGH_PERFORMANCE;
 attr.failIfMajorPerformanceCaveat=EM_FALSE;
 attr.majorVersion=v2;
 attr.minorVersion=v0;
-  emscripten_webgl_init_context_attributes(&attr);
-
+emscripten_webgl_init_context_attributes(&attr);
+  
 int S=EM_ASM_INT({return parseInt(window.innerHeight);});
 EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx=emscripten_webgl_create_context("#pcanvas",&attr);
 EGLConfig eglconfig=NULL;
@@ -123,10 +123,12 @@ eglMakeCurrent(display,surface,surface,contextegl);
 emscripten_webgl_make_context_current(ctx);
 int width=S;
 int height=S;
+
 SDL_Log("GL_VERSION: %s",glGetString(GL_VERSION));
 SDL_Log("GLSL_VERSION: %s",glGetString(GL_SHADING_LANGUAGE_VERSION));
 std::cout<<glGetString(GL_VERSION)<<"\n";
 std::cout<<glGetString(GL_SHADING_LANGUAGE_VERSION)<<"\n";
+
 app.settings.meshX=32;
 app.settings.meshY=32;
 app.settings.textureSize=512;
@@ -140,7 +142,7 @@ app.settings.aspectCorrection=false;
 app.settings.easterEgg=0;
 app.settings.shuffleEnabled=false;
 app.settings.presetURL="/presets";  
-app.settings.softCutRatingsEnabled = 1;
+app.settings.softCutRatingsEnabled=true;
 app.pm=new projectM(app.settings);
 printf("Init ProjectM\n");
 app.pm->selectRandom(true);
@@ -161,70 +163,6 @@ printf("%d\t%s\n",i,app.pm->getPresetName(i).c_str());
 //  glHint(GL_FRAGMENT_SHADER_DERIVATIVE_HINT,GL_NICEST);
 glClearColor(1.0,1.0,1.0,0.0);
 emscripten_set_main_loop((void (*)())renderFrame,0,0);
-}
-
-static void swtcht(){
-printf("Selecting random preset.\n");
-app.pm->selectRandom(true);
-}
-
-static void lckt(){
-app.pm->setPresetLock(true);
-printf("Preset locked.\n");
-}
-
-
-void cls_aud(){
-if(dev!=0){
-SDL_PauseAudioDevice(dev,SDL_TRUE);
-SDL_CloseAudioDevice(dev);
-dev=0;
-}}
-
-void qu(int rc){
-SDL_Quit();
-exit(rc);
-}
-
-void opn_aud(){
-dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
-if(!dev){
-SDL_FreeWAV(wave.snd);
-qu(2);
-}
-SDL_PauseAudioDevice(dev,SDL_FALSE);
-}
-
-void SDLCALL bfr(void *unused,Uint8* stm,int len){
-Uint8* wptr;
-int lft;
-wptr=wave.snd+wave.pos;
-lft=wave.slen-wave.pos;
-while (lft<=len){
-SDL_memcpy(stm,wptr,lft);
-stm+=lft;
-len-=lft;
-wptr=wave.snd;
-lft=wave.slen;
-wave.pos=0;
-}
-SDL_memcpy(stm,wptr,len);
-wave.pos+=len;
-}
-
-void plt(){
-// cls_aud();
-// SDL_FreeWAV(wave.snd);
-// SDL_Quit();
-SDL_SetMainReady();
-SDL_Init(SDL_INIT_AUDIO);
-SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
-if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
-qu(1);
-}
-wave.pos=0;
-wave.spec.callback=bfr;
-opn_aud();
 }
 
 EM_JS(void,ma,(),{
@@ -331,7 +269,69 @@ T=true;
 S();};return()=>{T=true;};}
 });
 
-  
+void swtcht(){
+printf("Selecting random preset.\n");
+app.pm->selectRandom(true);
+}
+
+void lckt(){
+app.pm->setPresetLock(true);
+printf("Preset locked.\n");
+}
+
+void cls_aud(){
+if(dev!=0){
+SDL_PauseAudioDevice(dev,SDL_TRUE);
+SDL_CloseAudioDevice(dev);
+dev=0;
+}}
+
+void qu(int rc){
+SDL_Quit();
+exit(rc);
+}
+
+void opn_aud(){
+dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){
+SDL_FreeWAV(wave.snd);
+qu(2);
+}
+SDL_PauseAudioDevice(dev,SDL_FALSE);
+}
+
+void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
+int lft;
+wptr=wave.snd+wave.pos;
+lft=wave.slen-wave.pos;
+while (lft<=len){
+SDL_memcpy(stm,wptr,lft);
+stm+=lft;
+len-=lft;
+wptr=wave.snd;
+lft=wave.slen;
+wave.pos=0;
+}
+SDL_memcpy(stm,wptr,len);
+wave.pos+=len;
+}
+
+void plt(){
+// cls_aud();
+// SDL_FreeWAV(wave.snd);
+// SDL_Quit();
+SDL_SetMainReady();
+SDL_Init(SDL_INIT_AUDIO);
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
+qu(1);
+}
+wave.pos=0;
+wave.spec.callback=bfr;
+opn_aud();
+}
+
 extern "C" {
   
 void pl(){
